a71778e89995812f0fa4cf3e6d9c66db5a30e713
khattrishashwat <shashwat.k@luckpay.in>
2026-05-04 17:06:31 +0530

    min 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // 🔥 ye line must hai
+  base: "/",  
 })
